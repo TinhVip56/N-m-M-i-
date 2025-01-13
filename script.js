971612486a79da6778a2afcc23a00a3e3a742a79
TinhVip <anhungvip7@gmail.com>
2025-01-13 20:05:29 +0700

    totinh

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-function jumpCapybara() {
-    const capybara = document.querySelector('.capybara');
-    capybara.classList.add('jump');
-    
-    // Khôi phục trạng thái sau 0.5s
-    setTimeout(() => {
-        capybara.classList.remove('jump');
-    }, 500);
+function showMessage() {
+    var message = document.getElementById('message');
+    message.style.opacity = 1;
+    // Thêm hiệu ứng "VIP" khi nhấn nút
+    var button = document.querySelector('button');
+    button.style.backgroundColor = "#ff0066"; // Màu sắc thay đổi khi nhấn
+    button.style.transform = "scale(0.95)";
+    setTimeout(function() {
+        button.style.transform = "scale(1)"; // Khôi phục lại kích thước sau khi nhấn
+    }, 100);
 }
